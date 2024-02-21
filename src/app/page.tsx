bceb908b20504b866493a51f126ff1fc5dbb1331
jakegilesphillips <jakegilesphillips@gmail.com>
2024-02-21 22:02:58 +0000

    Fix text sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,30 +8,30 @@ export default function Home() {
 
   return (
     <main className="flex flex-col justify-center items-center min-w-screen min-h-screen mono">
-      <div className="flex flex-col bg-zinc-900 px-10 pb-10 pt-6 overflow-hidden rounded-xl">
+      <div className="flex flex-col bg-zinc-900 px-6 pb-8 pt-4 overflow-hidden rounded-xl">
         <div className="flex flex-row justify-start items-center w-full px-3 py-6">
           <div className="flex flex-col mr-4 overflow-hidden rounded-lg">
-            <Image src={Logo} alt="" width={150} height={190} />
+            <Image src={Logo} alt="" width={80} height={80} />
           </div>
-          <span className="text-white text-[7em] px-4">DEPARTURES</span>
+          <span className="text-white text-[4em] px-4">DEPARTURES</span>
         </div>
-        <div className="flex flex-row justify-center items-center gap-20">
-          <div className="flex flex-col justify-center items-start gap-8 p-2">
-            <span className="text-white text-[4em]">ROOM</span>
+        <div className="flex flex-row justify-center items-center gap-10">
+          <div className="flex flex-col justify-center items-start gap-2 p-2">
+            <span className="text-white text-[2em]">ROOM</span>
             <SplitFlapRow word="SUITE 1" length={9} speed1={speed1} speed2={speed2} />
             <SplitFlapRow word="SUITE 2" length={9} speed1={speed1} speed2={speed2} />
             <SplitFlapRow word="MAIN HALL" length={9} speed1={speed1} speed2={speed2} />
             <SplitFlapRow word="BATHROOMS" length={9} speed1={speed1} speed2={speed2} />
           </div>
-          <div className="flex flex-col justify-center items-start gap-8 p-2">
-            <span className="text-white text-[4em]">CONFERENCE</span>
+          <div className="flex flex-col justify-center items-start gap-2 p-2">
+            <span className="text-white text-[2em]">CONFERENCE</span>
             <SplitFlapRow word="SOMETHING ABOUT CODE" length={20} speed1={speed1} speed2={speed2} type='ALPHABET' />
             <SplitFlapRow word="PYTHON IS NOT OK" length={20} speed1={speed1} speed2={speed2} type='ALPHABET' />
             <SplitFlapRow word="DATABASES FOR NERDS" length={20} speed1={speed1} speed2={speed2} type='ALPHABET' />
             <SplitFlapRow word="DUCK DUCK GOOSE" length={20} speed1={speed1} speed2={speed2} type='ALPHABET' />
           </div>
-          <div className="flex flex-col justify-center items-start gap-8 p-2">
-            <span className="text-white text-[4em]">TIME</span>
+          <div className="flex flex-col justify-center items-start gap-2 p-2">
+            <span className="text-white text-[2em]">TIME</span>
             <SplitFlapRow word="10:15" length={5} speed1={speed1} speed2={speed2} type='NUMERIC' />
             <SplitFlapRow word="11:10" length={5} speed1={speed1} speed2={speed2} type='NUMERIC' />
             <SplitFlapRow word="12:30" length={5} speed1={speed1} speed2={speed2} type='NUMERIC' />
